0f88a6f8dc4cda7958ec5c4f789bf08708204f35
peppapig-03 <245528390+peppapig-03@users.noreply.github.com>
2026-04-06 10:59:56 +0800

    added prompt at the top

--- main.js
+++ main.js
@@ -1,29 +1,42 @@
-const container=document.querySelector(".container")
-container.style.display="flex"
-container.style.flexDirection="column"
-for(let rowcount=0;rowcount<16;rowcount++){
-    const row=document.createElement("div")
-    row.classList.add("row")
-    row.style.display="flex"
-    row.style.gap="0"
-    row.style.justifyContent="center"
-    row.style.margin="0px"
-    row.style.padding="0px"
-    for(let columncount=0; columncount<16;columncount++){
-        const gridbox=document.createElement("div")
-        gridbox.classList.add("gridbox")
-        gridbox.style.padding="0px"
-        gridbox.style.margin="0px"
-        gridbox.style.border="1px solid black"
-        gridbox.style.width="50px"
-        gridbox.style.height="50px"
-        gridbox.addEventListener("mouseenter",(event)=>{
-            event.target.style.backgroundColor="blue"
-        })
-        gridbox.addEventListener("mouseleave",(event)=>{
-            event.target.style.backgroundColor="white"
-        })
-        row.appendChild(gridbox)
+const controller=document.querySelector("#controller")
+controller.addEventListener("click",(event)=>{
+    let result=parseInt(prompt("New Grid Size"))
+    generateNewGrid(result)
+})
+function generateNewGrid(number){
+    const container=document.querySelector(".container")
+    container.style.display="flex"
+    container.style.width="100%"
+    container.style.padding="0px"
+    container.style.flexDirection="column"
+    container.replaceChildren()
+    for(let rowcount=0;rowcount<number;rowcount++){
+        const row=document.createElement("div")
+        row.classList.add("row")
+        row.style.display="flex"
+        row.style.gap="0"
+        row.style.justifyContent="center"
+        row.style.margin="0px"
+        row.style.padding="0px"
+        row.style.width="100%"
+        row.style.height="70px;"
+        for(let columncount=0; columncount<number;columncount++){
+            const gridbox=document.createElement("div")
+            gridbox.classList.add("gridbox")
+            gridbox.style.padding="0px"
+            gridbox.style.margin="0px"
+            gridbox.style.border="1px solid black"
+            gridbox.style.width=`${100/number}%`
+            gridbox.style.aspectRatio="1/1"
+            gridbox.addEventListener("mouseenter",(event)=>{
+                event.target.style.backgroundColor="blue"
+            })
+            gridbox.addEventListener("mouseleave",(event)=>{
+                event.target.style.backgroundColor="white"
+            })
+            row.appendChild(gridbox)
+        }
+        container.appendChild(row)
     }
-    container.appendChild(row)
 }
+generateNewGrid(20)
